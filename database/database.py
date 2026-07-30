bf81c1bb30832fb28057e9bb2207a54f12f87338
Akshay Kumar <akshaykumar0891@users.noreply.github.com>
2026-07-30 17:55:06 +0530

    Debug DATABASE_URL

--- database/database.py
+++ database/database.py
@@ -21,10 +21,6 @@ if DATABASE_URL.startswith("postgres://"):
 connect_args = {}
 if DATABASE_URL.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
-print("=" * 60)
-print("DATABASE_URL repr:")
-print(repr(DATABASE_URL))
-print("=" * 60)
 
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
